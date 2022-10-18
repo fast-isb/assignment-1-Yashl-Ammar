7b5ceda67cd87039719e779f2d6a25a52a0c6a1e
Yashl Ammar <78467616+Yashl-Ammar@users.noreply.github.com>
2022-10-18 13:22:40 +0500

    worker sign up basic structure

---
+++ frontend/src/components/WorkerSignUp.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./WorkerSignUp.css"
+
+class WorkerSignUp extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <div className="center">
+                    <h3>First Name:</h3>
+                    <input />
+                    <h3>Last Name:</h3>
+                    <input />
+                    <h3>Date of Birth:</h3>
+                    <input />
+                    <h3>Phone Number:</h3>
+                    <input />
+                    <br/>
+                    <button>Next</button>
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default WorkerSignUp;
