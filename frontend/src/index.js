32b0bdf8beafbf966e18742abd35d04afb124eeb
Yashl Ammar <78467616+Yashl-Ammar@users.noreply.github.com>
2022-10-20 02:51:35 +0500

    fetching data for search worker feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import AdminSearchWorker from "./components/AdminSearchWorker.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkerSignUp />
+    <AdminSearchWorker />
   </React.StrictMode>
 );
 
