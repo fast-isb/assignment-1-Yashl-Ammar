9d72e747c44112140a5007c5da44453cc527a7a4
SHEIKH MUHAMMAD SAAD <81197058+SHEIKHSAAD2561@users.noreply.github.com>
2022-10-19 13:24:25 -0700

    backend ,Home and Login Updated

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,28 @@
-import express from 'express';
+import express from "express";
 
 // const express = require('express');
 const router = express.Router();
 
 const users = [
-    {
-        name: 'John Doe',
-        age: 25
-    }
-]
+  {
+    name: "John Doe",
+    age: 25,
+  },
+];
 
-router.get('/', (req, res) => {
-    res.send(users)
-})
+router.get("/", (req, res) => {
+  res.send(users);
+});
 
-router.post('/', (req, res) => {
-    const user = req.body;
-    console.log(req.body);
-    res.send(`added into the DB`)
-})
+app.post("/login", (req, res) => {
+  res.send("Hello Universe!");
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  console.log(req.body);
+  res.send(`added into the DB`);
+});
 
 export default router;
 
