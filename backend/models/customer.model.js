5446ef7b5f49367c02bac796e2faef95d7bd1cb8
wajahat Ullah <i201769@nu.edu.pk>
2022-10-26 22:34:08 +0500

    Database Connected

---
+++ backend/models/customer.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const Schema= mongoose.Schema
+
+const customerSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:false,
+    },
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:false,
+    },
+    phoneNum:{
+        type: String, 
+        required: true, 
+        unique: false, 
+    },
+    Address:{
+        type:String,
+        required:true,
+        unique:false,
+    },
+
+
+},
+{
+    timestamps:true
+})
+
+const Customer=mongoose.model("Customer",customerSchema)
+
+export default Customer
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import Customer from "../models/customer.model.js";
 // const express = require('express');
 const router = express.Router();
 
@@ -22,11 +23,41 @@ router.post("/", (req, res) => {
 
 router.post("/userSignIn",(req,res)=>{
     console.log(req.body)
+    
+  
 })
 
 router.post("/userSignUp",(req,res)=>{
     console.log(req.body)
-})
+    let name=req.body.name
+    let userName=req.body.username
+    let email= req.body.email
+    let password=req.body.password
+    let Address=req.body.address
+    let phoneNum= req.body.phoneNum
+
+    const newUser= new Customer({
+    name,
+    userName,
+    email,
+    password,
+    phoneNum,
+    Address,
+    });
+
+   newUser
+   .save()
+    .then(() => {
+      res.json("Worker has been added");
+      
+    })
+    .catch((err) => {
+      res.status(400).json("Error " + err);
+      
+    });
+
+   
+});
 
 export default router;
 
